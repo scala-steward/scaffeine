65133294bfd14e82ea7199e9212c55f19cb3ba5f
Bastien LEMALE <bastien.lemale@teads.tv>
2016-04-06 12:31:37 +0200

    Add scaladoc for AsyncLoadingCache

--- src/main/scala/com/github/blemale/scaffeine/AsyncLoadingCache.scala
+++ src/main/scala/com/github/blemale/scaffeine/AsyncLoadingCache.scala
@@ -15,31 +15,94 @@ object AsyncLoadingCache {
 }
 
 class AsyncLoadingCache[K, V](val underlying: CaffeineAsyncLoadingCache[K, V]) {
-
+  /**
+   * Returns the future associated with `key` in this cache, or `None` if there is no
+   * cached future for `key`.
+   *
+   * @param key key whose associated value is to be returned
+   * @return an option containing the current (existing or computed) future value to which the
+   *         specified key is mapped, or `None` if this map contains no mapping for the key
+   */
   def getIfPresent(key: K)(implicit ec: ExecutionContext): Future[Option[V]] =
     Option(underlying.getIfPresent(key)) match {
       case Some(futureValue) => futureValue.toScala.map(Some(_))
       case None => Future.successful(None)
     }
 
+  /**
+   * Returns the future associated with `key` in this cache, obtaining that value from
+   * `mappingFunction` if necessary. This method provides a simple substitute for the
+   * conventional "if cached, return; otherwise create, cache and return" pattern.
+   *
+   * @param key             key with which the specified value is to be associated
+   * @param mappingFunction the function to asynchronously compute a value
+   * @return the current (existing or computed) future value associated with the specified key
+   */
   def get(key: K, mappingFunction: K => V)(implicit ec: ExecutionContext): Future[V] =
     underlying.get(key, asJavaFunction(mappingFunction)).toScala
 
+  /**
+   * Returns the future associated with `key` in this cache, obtaining that value from
+   * `mappingFunction` if necessary. This method provides a simple substitute for the
+   * conventional "if cached, return; otherwise create, cache and return" pattern.
+   *
+   * @param key             key with which the specified value is to be associated
+   * @param mappingFunction the function to asynchronously compute a value
+   * @return the current (existing or computed) future value associated with the specified key
+   * @throws java.lang.RuntimeException     or Error if the mappingFunction does when constructing the future,
+   *                              in which case the mapping is left unestablished
+   */
   def getFuture(key: K, mappingFunction: K => Future[V])(implicit ec: ExecutionContext): Future[V] =
     underlying.get(
       key,
       asJavaBiFunction((k: K, _: Executor) => mappingFunction(k).toJava.toCompletableFuture)
     ).toScala
 
+  /**
+   * Returns the future associated with `key` in this cache, obtaining that value from
+   * `loader` if necessary. If the asynchronous computation fails, the entry
+   * will be automatically removed.
+   *
+   * @param key key with which the specified value is to be associated
+   * @return the current (existing or computed) future value associated with the specified key
+   * @throws java.lang.RuntimeException     or Error if the `loader` does when constructing the future,
+   *                                                      in which case the mapping is left unestablished
+   */
   def get(key: K)(implicit ec: ExecutionContext): Future[V] =
     underlying.get(key).toScala
 
+  /**
+   * Returns the future of a map of the values associated with `keys`, creating or retrieving
+   * those values if necessary. The returned map contains entries that were already cached, combined
+   * with newly loaded entries. If the any of the asynchronous computations fail, those entries will
+   * be automatically removed.
+   *
+   * @param keys the keys whose associated values are to be returned
+   * @return the future containing an mapping of keys to values for the specified keys in this cache
+   * @throws java.lang.RuntimeException     or Error if the `loader` does so
+   */
   def getAll(keys: Iterable[K])(implicit ec: ExecutionContext): Future[Map[K, V]] =
     underlying.getAll(keys.asJava).toScala.map(_.asScala.toMap)
 
+  /**
+   * Associates `value` with `key` in this cache. If the cache previously contained a
+   * value associated with `key`, the old value is replaced by `value`. If the
+   * asynchronous computation fails, the entry will be automatically removed.
+   *
+   * @param key         key with which the specified value is to be associated
+   * @param valueFuture value to be associated with the specified key
+   */
   def put(key: K, valueFuture: Future[V])(implicit ec: ExecutionContext): Unit =
     underlying.put(key, valueFuture.toJava.toCompletableFuture)
 
+  /**
+   * Returns a view of the entries stored in this cache as a synchronous [[LoadingCache]]. A
+   * mapping is not present if the value is currently being loaded. Modifications made to the
+   * synchronous cache directly affect the asynchronous cache. If a modification is made to a
+   * mapping that is currently loading, the operation blocks until the computation completes.
+   *
+   * @return a thread-safe synchronous view of this cache
+   */
   def synchronous(): LoadingCache[K, V] =
     LoadingCache(underlying.synchronous())
 
