4985916facbbada9a149770f9286a07c492cf7ba
Bastien LEMALE <blemale@protonmail.ch>
2019-01-20 20:52:54 +0100

    Bump dependencies
    * scala-java8-compat to 0.9.0
    * jsr305 to 3.0.2
    * scalatest & scalactic to 3.0.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,8 +4,8 @@ object Dependencies {
   val CaffeineVersion = "2.6.2"
 
   val Caffeine = "com.github.ben-manes.caffeine" % "caffeine" % CaffeineVersion
-  val Java8Compat = "org.scala-lang.modules" %% "scala-java8-compat" % "0.8.0"
-  val Jsr305 = "com.google.code.findbugs" % "jsr305" % "3.0.1"
-  val Scalactic = "org.scalactic" %% "scalactic" % "3.0.1"
-  val Scalatest = "org.scalatest" %% "scalatest" % "3.0.1"
+  val Java8Compat = "org.scala-lang.modules" %% "scala-java8-compat" % "0.9.0"
+  val Jsr305 = "com.google.code.findbugs" % "jsr305" % "3.0.2"
+  val Scalactic = "org.scalactic" %% "scalactic" % "3.0.5"
+  val Scalatest = "org.scalatest" %% "scalatest" % "3.0.5"
 }
